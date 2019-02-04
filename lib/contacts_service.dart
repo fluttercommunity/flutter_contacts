ec402910aebcf0b2a7a7887a0157613167002ba2
Lukas Dickie <lukasgit@users.noreply.github.com>
2019-02-03 18:48:02 -0800

    Update contacts_service.dart

--- lib/contacts_service.dart
+++ lib/contacts_service.dart
@@ -7,7 +7,7 @@ export 'share.dart';
 
 class ContactsService {
   static const MethodChannel _channel =
-      MethodChannel('github.com/clovisnicolas/flutter_contacts');
+      MethodChannel('github.com/fluttercommunity/flutter_contacts');
 
   /// Fetches all contacts, or when specified, the contacts with a name
   /// matching [query]
