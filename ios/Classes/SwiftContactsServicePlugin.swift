2d78831def95cb8ef2ad33c44726fa99cb31da1c
Marko Perutović <marko.perutovic@gmail.com>
2019-06-19 20:54:56 +0200

    android contact photo high resolution

--- ios/Classes/SwiftContactsServicePlugin.swift
+++ ios/Classes/SwiftContactsServicePlugin.swift
@@ -14,7 +14,12 @@ public class SwiftContactsServicePlugin: NSObject, FlutterPlugin {
         switch call.method {
         case "getContacts":
             let arguments = call.arguments as! [String:Any]
-            result(getContacts(query: (arguments["query"] as? String), withThumbnails: arguments["withThumbnails"] as! Bool))
+            result(getContacts(query: (arguments["query"] as? String), withThumbnails: arguments["withThumbnails"] as! Bool,
+                               photoHighResolution: arguments["photoHighResolution"] as! Bool, phoneQuery:  false))
+        case "getContactsForPhone":
+            let arguments = call.arguments as! [String:Any]
+            result(getContacts(query: (arguments["phone"] as? String), withThumbnails: arguments["withThumbnails"] as! Bool,
+                               photoHighResolution: arguments["photoHighResolution"] as! Bool, phoneQuery:  true))
         case "addContact":
             let contact = dictionaryToContact(dictionary: call.arguments as! [String : Any])
 
@@ -43,41 +48,102 @@ public class SwiftContactsServicePlugin: NSObject, FlutterPlugin {
             result(FlutterMethodNotImplemented)
         }
     }
-    
-    func getContacts(query : String?, withThumbnails: Bool) -> [[String:Any]]{
+//    func searchForContactUsingPhoneNumber(phoneNumber: String) {
+//
+//        dispatch_async(dispatch_get_global_queue(QOS_CLASS_USER_INTERACTIVE, 0), { () -> Void in
+//            self.requestForAccess { (accessGranted) -> Void in
+//                if accessGranted {
+//                    let keys = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactImageDataKey, CNContactPhoneNumbersKey]
+//                    var contacts = [CNContact]()
+//                    var message: String!
+//
+//                    let contactsStore = CNContactStore()
+//                    do {
+//                        try contactsStore.enumerateContactsWithFetchRequest(CNContactFetchRequest(keysToFetch: keys)) {
+//                            (contact, cursor) -> Void in
+//                            if (!contact.phoneNumbers.isEmpty) {
+//                                let phoneNumberToCompareAgainst = phoneNumber.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet).joinWithSeparator("")
+//                                for phoneNumber in contact.phoneNumbers {
+//                                    if let phoneNumberStruct = phoneNumber.value as? CNPhoneNumber {
+//                                        let phoneNumberString = phoneNumberStruct.stringValue
+//                                        let phoneNumberToCompare = phoneNumberString.componentsSeparatedByCharactersInSet(NSCharacterSet.decimalDigitCharacterSet().invertedSet).joinWithSeparator("")
+//                                        if phoneNumberToCompare == phoneNumberToCompareAgainst {
+//                                            contacts.append(contact)
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                        }
+//
+//                        if contacts.count == 0 {
+//                            message = "No contacts were found matching the given phone number."
+//                        }
+//                    }
+//                    catch {
+//                        message = "Unable to fetch contacts."
+//                    }
+//
+//                    if message != nil {
+//                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                            self.showMessage(message)
+//                        })
+//                    }
+//                    else {
+//                        // Success
+//                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                            // Do someting with the contacts in the main queue, for example
+//                            /*
+//                             self.delegate.didFetchContacts(contacts) <= which extracts the required info and puts it in a tableview
+//                             */
+//                            print(contacts) // Will print all contact info for each contact (multiple line is, for example, there are multiple phone numbers or email addresses)
+//                            let contact = contacts[0] // For just the first contact (if two contacts had the same phone number)
+//                            print(contact.givenName) // Print the "first" name
+//                            print(contact.familyName) // Print the "last" name
+//                            if contact.isKeyAvailable(CNContactImageDataKey) {
+//                                if let contactImageData = contact.imageData {
+//                                    print(UIImage(data: contactImageData)) // Print the image set on the contact
+//                                }
+//                            } else {
+//                                // No Image available
+//
+//                            }
+//                        })
+//                    }
+//                }
+//            }
+//        })
+//    }
+
+    func getContacts(query : String?, withThumbnails: Bool, photoHighResolution: Bool, phoneQuery: Bool) -> [[String:Any]]{
         var contacts : [CNContact] = []
         //Create the store, keys & fetch request
         let store = CNContactStore()
-        let keys = withThumbnails == true
-            ? [CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
-               CNContactEmailAddressesKey,
-               CNContactPhoneNumbersKey,
-               CNContactFamilyNameKey,
-               CNContactGivenNameKey,
-               CNContactMiddleNameKey,
-               CNContactNamePrefixKey,
-               CNContactNameSuffixKey,
-               CNContactPostalAddressesKey,
-               CNContactOrganizationNameKey,
-               CNContactThumbnailImageDataKey,
-               CNContactNoteKey,
-               CNContactJobTitleKey] as [Any]
-            : [CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
-               CNContactEmailAddressesKey,
-               CNContactPhoneNumbersKey,
-               CNContactFamilyNameKey,
-               CNContactGivenNameKey,
-               CNContactMiddleNameKey,
-               CNContactNamePrefixKey,
-               CNContactNameSuffixKey,
-               CNContactPostalAddressesKey,
-               CNContactOrganizationNameKey,
-               CNContactNoteKey,
-               CNContactJobTitleKey] as [Any]
+        var keys = [CNContactFormatter.descriptorForRequiredKeys(for: .fullName),
+                    CNContactEmailAddressesKey,
+                    CNContactPhoneNumbersKey,
+                    CNContactFamilyNameKey,
+                    CNContactGivenNameKey,
+                    CNContactMiddleNameKey,
+                    CNContactNamePrefixKey,
+                    CNContactNameSuffixKey,
+                    CNContactPostalAddressesKey,
+                    CNContactOrganizationNameKey,
+                    CNContactNoteKey,
+                    CNContactJobTitleKey] as [Any]
+        
+        if(withThumbnails){
+            if(photoHighResolution){
+                keys.append(CNContactImageDataKey)
+            } else {
+                keys.append(CNContactThumbnailImageDataKey)
+            }
+        }
+        
         let fetchRequest = CNContactFetchRequest(keysToFetch: keys as! [CNKeyDescriptor])
         // Set the predicate if there is a query
         if let query = query{
             fetchRequest.predicate = CNContact.predicateForContacts(matchingName: query)
+            
         }
         // Fetch contacts
         do{
@@ -164,6 +230,7 @@ public class SwiftContactsServicePlugin: NSObject, FlutterPlugin {
                 contact.organizationName = dictionary["company"] as? String ?? ""
                 contact.jobTitle = dictionary["jobTitle"] as? String ?? ""
                 contact.note = dictionary["note"] as? String ?? ""
+                contact.imageData = (dictionary["avatar"] as? FlutterStandardTypedData)?.data
 
                 //Phone numbers
                 if let phoneNumbers = dictionary["phones"] as? [[String:String]]{
@@ -278,6 +345,11 @@ public class SwiftContactsServicePlugin: NSObject, FlutterPlugin {
         result["note"] = contact.note
         if contact.isKeyAvailable(CNContactThumbnailImageDataKey) {
             if let avatarData = contact.thumbnailImageData {
+                result["avatar"] = FlutterStandardTypedData(bytes: avatarData)
+            }
+        }
+        if contact.isKeyAvailable(CNContactImageDataKey) {
+            if let avatarData = contact.imageData {
                 result["avatar"] = FlutterStandardTypedData(bytes: avatarData)
             }
         }
